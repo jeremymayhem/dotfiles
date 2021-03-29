e20acacd7a9ba4a01c971326081da54009da21ac
Jeremy Mayhew <jeremy.d.mayhew@systolic.com>
2021-03-29 13:46:17 -0400

    Adding helm tab completion

--- bashrc.better.d/kubernetes.bash
+++ bashrc.better.d/kubernetes.bash
@@ -7,3 +7,7 @@ complete -F __start_kubectl k
 source <(minikube completion bash)
 alias mk=minikube
 complete -F __start_minikube mk
+
+source <(helm completion bash)
+alias h=helm
+complete -F __start_helm h
