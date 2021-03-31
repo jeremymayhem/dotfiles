c137f8123b73f8b02636cf0fc684d6081f8c7885
Jeremy Mayhew <jeremy.d.mayhew@systolic.com>
2021-03-31 21:59:40 +0000

    Kubecolor, git stagediff, and bashrc idempotentcy

--- bashrc.better.d/kubernetes.bash
+++ bashrc.better.d/kubernetes.bash
@@ -4,6 +4,14 @@ source <(kubectl completion bash)
 alias k=kubectl
 complete -F __start_kubectl k
 
+# Don't want colors?  Add this to .bashrc.config:
+# export KUBECOLOR_ARGS=--plain
+#
+# Have a white background?  Yuck.  Add this to .bashrc.config:
+# export KUBECOLOR_ARGS=--light-background
+command -v kubecolor >/dev/null 2>&1 && alias kubectl="kubecolor ${KUBECOLOR_ARGS}"
+command -v kubecolor >/dev/null 2>&1 && complete -F __start_kubectl kubecolor
+
 source <(minikube completion bash)
 alias mk=minikube
 complete -F __start_minikube mk
